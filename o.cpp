9002255f382c8c159f6d42bec975b2062ffcb491
Nerg <mas.ikhwanalhakim@gmail.com>
2023-02-24 08:14:35 +0700

    penambahan fungsi update distance

--- o.cpp
+++ o.cpp
@@ -7,24 +7,30 @@ const int map_size = 10;
 class ship {
         private:
             string name;
-            int health, damage, range, x, y, xDist, yDist;
+            int health, damage, range;
         public:
+            int x, y, xDist, yDist;
             void input(string, int, int, int, int, int, int, int);
             void output();
             void forward();
             void backward();
             void right();
             void left();
+            void updateDist(int, int);
+            bool isDead();
 };
 int main()
 {
     srand(time(0));
     ship cakru, musuh;
     cakru.input("Cakru", 10, 2, 2, 0, 0, 0, 0);
+    musuh.input("Musuh", 4, 1, 2, -5 + (rand() % 11), -5 + (rand() % 11), 0, 0);
+    cakru.updateDist(musuh.x, musuh.y);
+    musuh.updateDist(cakru.x, cakru.y);
     cakru.output();
     cout << endl;
-    musuh.input("Musuh", 4, 1, 2, -5 + (rand() % 11), -5 + (rand() % 11), 0 , 0);
     musuh.output();
+    cout << endl;
 }
 
 void ship::input(string n, int h, int d, int r, int posX, int posY, int xD, int yD)
@@ -46,7 +52,8 @@ void ship::output()
     cout << "Damage: " << damage << endl;
     cout << "Jarak Serangan: " << range << endl;
     cout << "Posisi: " << "(" << x << "," << y << ")" << endl;
-    cout << "Jarak dengan Lawan: " << "(" << xDist << "," << yDist << ")" << endl;
+    cout << "Jarak dengan Musuh: " << "(" << xDist << "," << yDist << ")" << endl;
+
 }
 
 void ship::forward()
@@ -67,4 +74,22 @@ void ship::right()
 void ship::left()
 {
     x-=1;
+}
+
+bool ship::isDead()
+{
+    if (health <=0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void ship::updateDist(int xEnemy, int yEnemy)
+{
+    xDist = xEnemy - x;
+    yDist = yEnemy - y;
 }
