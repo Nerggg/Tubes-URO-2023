6194ad625421576841f04ca2d7b5dab8564d8389
Nerg <mas.ikhwanalhakim@gmail.com>
2023-02-24 09:34:10 +0700

    penambahan algoritma serang dan bergerak

--- o.cpp
+++ o.cpp
@@ -3,14 +3,13 @@
 #include <ctime>
 #include <cstdlib>
 using namespace std;
-const int map_size = 10;
 class ship {
         private:
             string name;
-            int health, damage, range;
+            int health;
         public:
-            int x, y, xDist, yDist;
-            void input(string, int, int, int, int, int, int, int);
+            int damage, range, x, y, xDist, yDist;
+            void input(string, int, int, int, int, int);
             void output();
             void forward();
             void backward();
@@ -18,22 +17,94 @@ class ship {
             void left();
             void updateDist(int, int);
             bool isDead();
+            void takeDamage(int);
 };
+int input;
 int main()
 {
     srand(time(0));
     ship cakru, musuh;
-    cakru.input("Cakru", 10, 2, 2, 0, 0, 0, 0);
-    musuh.input("Musuh", 4, 1, 2, -5 + (rand() % 11), -5 + (rand() % 11), 0, 0);
-    cakru.updateDist(musuh.x, musuh.y);
-    musuh.updateDist(cakru.x, cakru.y);
-    cakru.output();
-    cout << endl;
-    musuh.output();
-    cout << endl;
+    cakru.input("Cakru", 10, 2, 2, 0, 0);
+    musuh.input("Musuh", 4, 1, 2, -5 + (rand() % 11), -5 + (rand() % 11));
+    while (cakru.isDead() == false)
+    {
+        system("cls");
+        cakru.updateDist(musuh.x, musuh.y);
+        musuh.updateDist(cakru.x, cakru.y);
+        cakru.output();
+        cout << endl;
+        musuh.output();
+        cout << endl;
+        cout << "Sekarang adalah giliran Anda. Apa yang ingin Anda lakukan?" << endl;
+        cout << "[1] Bergerak maju\n[2] Bergerak mundur\n[3] Bergerak ke kanan\n[4] Bergerak ke kiri\n[5] Serang musuh\n[6] Diam di tempat\n";
+        cin >> input;
+        switch (input)
+        {
+            case 1:
+                cakru.forward();
+                break;
+            case 2:
+                cakru.backward();
+                break;
+            case 3:
+                cakru.right();
+                break;
+            case 4:
+                cakru.left();
+                break;
+            case 5:
+                if (abs(cakru.xDist) <= 2 && abs(cakru.yDist) <= 2)
+                {
+                    cout << "Serangan kena!" << endl;
+                    musuh.takeDamage(cakru.damage);
+                    system("pause");
+                }
+                else
+                {
+                    cout << "Yah serangan tidak kena!" << endl;
+                    system("pause");
+                }
+                break;
+            case 6:
+                break;
+        }
+        cakru.updateDist(musuh.x, musuh.y);
+        musuh.updateDist(cakru.x, cakru.y);
+        cout << "Sekarang adalah giliran musuh" << endl;
+        if (abs(musuh.xDist) <= 2 && abs(musuh.yDist) <= 2)
+        {
+            cakru.takeDamage(musuh.damage);
+            cout << "Musuh menyerang kapal Cakru!" << endl;
+            system("pause");
+        }
+        else if (musuh.xDist > musuh.range)
+        {
+            musuh.right();
+            cout << "Musuh bergerak ke kanan" << endl;
+            system("pause");
+        }
+        else if (musuh.xDist < -1*musuh.range)
+        {
+            musuh.left();
+            cout << "Musuh bergerak ke kiri" << endl;
+            system("pause");
+        }
+        else if (musuh.yDist > musuh.range)
+        {
+            musuh.forward();
+            cout << "Musuh bergerak maju" << endl;
+            system("pause");
+        }
+        else if (musuh.yDist < -1*musuh.range)
+        {
+            musuh.backward();
+            cout << "Musuh bergerak mundur" << endl;
+            system("pause");
+        }
+    }
 }
 
-void ship::input(string n, int h, int d, int r, int posX, int posY, int xD, int yD)
+void ship::input(string n, int h, int d, int r, int posX, int posY)
 {
     name = n;
     health = h;
@@ -41,8 +112,6 @@ void ship::input(string n, int h, int d, int r, int posX, int posY, int xD, int 
     range = r;
     x = posX;
     y = posY;
-    xDist = xD;
-    yDist = yD;
 }
 
 void ship::output()
@@ -92,4 +161,9 @@ void ship::updateDist(int xEnemy, int yEnemy)
 {
     xDist = xEnemy - x;
     yDist = yEnemy - y;
+}
+
+void ship::takeDamage(int damage)
+{
+    health-=damage;
 }
