9b23cdad2a9040be69ed9eb3bfa8896e49e05a05
Nerg <mas.ikhwanalhakim@gmail.com>
2023-02-24 10:49:20 +0700

    perbaikan bug dan optimasi kode

--- o.cpp
+++ o.cpp
@@ -4,46 +4,46 @@
 #include <cstdlib>
 using namespace std;
 class ship {
-        private:
-            string name;
-            int health;
-        public:
-            int damage, range, x, y, xDist, yDist;
-            void input(string, int, int, int, int, int);
-            void output();
-            void forward();
-            void backward();
-            void right();
-            void left();
-            void updateDist(int, int);
-            bool isDead();
-            void takeDamage(int);
+    private:
+        string name;
+        int health;
+    public:
+        int damage, range, x, y, xDist, yDist;
+        void input(string, int, int, int, int, int);
+        void output();
+        void forward();
+        void backward();
+        void right();
+        void left();
+        void updateDist(int, int);
+        bool isDead();
+        void takeDamage(int);
 };
 void printMap(int, int, int, int);
-int input, killCount;
+int input, diff, killCount = 0;
 int main()
 {
     srand(time(0));
     ship cakru, musuh;
     cout << "Pilih tingkat kesulitan" << endl;
     cout << "[1] Mudah\n[2] Normal\n[3] Sulit\n[4] Sulit Banget\n";
-    cin >> input;
-    if (input == 1)
+    cin >> diff;
+    if (diff == 1)
     {
         cakru.input("Cakru", 10, 2, 2, 0, 0);
         musuh.input("Musuh", 4, 1, 2, -5 + (rand() % 11), -5 + (rand() % 11));       
     }
-    else if (input == 2)
+    else if (diff == 2)
     {
         cakru.input("Cakru", 10, 2, 2, 0, 0);
         musuh.input("Musuh", 4, 2, 2, -5 + (rand() % 11), -5 + (rand() % 11));
     }
-    else if (input == 3)
+    else if (diff == 3)
     {
         cakru.input("Cakru", 10, 2, 2, 0, 0);
         musuh.input("Musuh", 4, 3, 2, -5 + (rand() % 11), -5 + (rand() % 11));
     }
-    else if (input == 4)
+    else if (diff == 4)
     {
         cakru.input("Cakru", 10, 2, 2, 0, 0);
         musuh.input("Musuh", 4, 3, 3, -5 + (rand() % 11), -5 + (rand() % 11));
@@ -96,44 +96,59 @@ int main()
         musuh.updateDist(cakru.x, cakru.y);
         if (musuh.isDead() == false)
         {
-        cout << "Sekarang adalah giliran musuh" << endl;
-        if (abs(musuh.xDist) <= 2 && abs(musuh.yDist) <= 2)
-        {
-            cakru.takeDamage(musuh.damage);
-            cout << "Musuh menyerang kapal Cakru!" << endl;
-            system("pause");
-        }
-        else if (musuh.xDist > musuh.range)
-        {
-            musuh.right();
-            cout << "Musuh bergerak ke kanan" << endl;
-            system("pause");
-        }
-        else if (musuh.xDist < -1*musuh.range)
-        {
-            musuh.left();
-            cout << "Musuh bergerak ke kiri" << endl;
-            system("pause");
-        }
-        else if (musuh.yDist > musuh.range)
-        {
-            musuh.forward();
-            cout << "Musuh bergerak maju" << endl;
-            system("pause");
-        }
-        else if (musuh.yDist < -1*musuh.range)
-        {
-            musuh.backward();
-            cout << "Musuh bergerak mundur" << endl;
-            system("pause");
-        }
+            cout << "Sekarang adalah giliran musuh" << endl;
+            if (abs(musuh.xDist) <= musuh.range && abs(musuh.yDist) <= musuh.range)
+            {
+                cakru.takeDamage(musuh.damage);
+                cout << "Musuh menyerang kapal Cakru!" << endl;
+                system("pause");
+            }
+            else if (musuh.xDist > musuh.range)
+            {
+                musuh.right();
+                cout << "Musuh bergerak ke kanan" << endl;
+                system("pause");
+            }
+            else if (musuh.xDist < -1*musuh.range)
+            {
+                musuh.left();
+                cout << "Musuh bergerak ke kiri" << endl;
+                system("pause");
+            }
+            else if (musuh.yDist > musuh.range)
+            {
+                musuh.forward();
+                cout << "Musuh bergerak maju" << endl;
+                system("pause");
+            }
+            else if (musuh.yDist < -1*musuh.range)
+            {
+                musuh.backward();
+                cout << "Musuh bergerak mundur" << endl;
+                system("pause");
+            }
         }
         else
         {
             cout << "Selamat! Kapal musuh telah hancur!" << endl;
             cout << "Oh tidak! Kapal musuh datang lagi!" << endl;
             killCount++;
-            musuh.input("Musuh", 4, 1, 2, -5 + (rand() % 11), -5 + (rand() % 11));
+            if (diff == 1)
+            {
+                musuh.input("Musuh", 4, 1, 2, -5 + (rand() % 11), -5 + (rand() % 11));       
+            }
+            else if (diff == 2)
+            {
+                musuh.input("Musuh", 4, 2, 2, -5 + (rand() % 11), -5 + (rand() % 11));
+            }
+            else if (diff == 3)
+            {
+                musuh.input("Musuh", 4, 3, 2, -5 + (rand() % 11), -5 + (rand() % 11));
+            }
+            else if (diff == 4)
+            {
+                musuh.input("Musuh", 4, 3, 3, -5 + (rand() % 11), -5 + (rand() % 11));
+            }
             system("pause");
         }
     }
